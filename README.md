d8702dc153b7e521dfeed601eb7562070f53ac95
Max Goodhart <c@chromakode.com>
2020-05-31 22:59:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Stream List Updater
+
+A script to update the status of live streams listed in the [2020 George Floyd Protest Tracker](http://bit.ly/protestlinks) by [twitch.tv/woke](https://twitch.tv/woke).
+
+## Installation
+
+1. Run `npm install`
+2. Create a [service account](https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication) and grant it edit access to the sheet.
+
+## Usage
+
+```
+SHEET_ID=<sheets id from url> npm start
+```
---
+++ index.js
@@ -0,0 +1,103 @@
+const {promisify} = require('util')
+const keyBy = require('lodash/keyBy')
+const cheerio = require('cheerio')
+const {GoogleSpreadsheet} = require('google-spreadsheet')
+const moment = require('moment-timezone')
+const fetch = require('node-fetch')
+const {default: PQueue} = require('p-queue')
+
+const SHEET_ID = process.env.SHEET_ID
+const CREDS = require('./creds.json')
+
+const sleep = promisify(setTimeout)
+
+function findString(platformName, string) {
+  return async function (url) {
+    const res = await fetch(url)
+    const text = await res.text()
+    const isLive = text.includes(string)
+    const $ = cheerio.load(text)
+    const title = $('title').text()
+    return {url, isLive, title, platformName}
+  }
+}
+
+const checkYTLive = findString('YouTube', `liveStreamability`)
+const checkFBLive = findString('Facebook', `"broadcast_status":"LIVE"`)
+const checkTwitchLive = findString('Twitch', `"isLiveBroadcast":true`)
+const checkPeriscopeLive = findString('Periscope', `name="twitter:text:broadcast_state" content="RUNNING"/>`)
+
+function checkForStream(url) {
+  if (url.startsWith('https://www.youtube.com') || url.startsWith('https://youtu.be')) {
+    return checkYTLive
+  } else if (url.startsWith('https://www.facebook.com/watch/live')) {
+    return checkFBLive
+  } else if (url.startsWith('https://www.twitch.tv')) {
+    return checkTwitchLive
+  } else if (url.startsWith('https://www.periscope.tv')) {
+    return checkPeriscopeLive
+  }
+}
+
+async function updateRow(row) {
+  const {Link} = row
+  const check = checkForStream(row.Link)
+  if (!check) {
+    return
+  }
+
+  const result = await check(row.Link)
+
+  row.Status = result.isLive ? 'Live' : 'Offline'
+  row['Title'] = result.title
+  row['Last Checked (CST)'] = moment().tz("America/Chicago").format('M/DD/YY HH:mm:ss')
+  await row.save()
+}
+
+async function main() {
+  const doc = new GoogleSpreadsheet(SHEET_ID)
+  await doc.useServiceAccountAuth(CREDS)
+  await doc.loadInfo()
+  const sheet = doc.sheetsByIndex[0]
+  const rows = await sheet.getRows()
+
+  const queue = new PQueue({concurrency: 10, autoStart: false})
+
+  function tryRow(row, tries=0) {
+    return async function() {
+      await sleep(tries * 5000)
+      try {
+        await updateRow(row)
+      } catch (err) {
+        if (err.response && err.response.status === 429) {
+          queue.pause()
+          console.log('ratelimited. waiting 5s...')
+          await sleep(5000)
+          queue.start()
+        } else {
+          console.warn('error updating row', row.Link, err.toString())
+        }
+
+        if (tries > 3) {
+          console.warn('giving up on row', row.Link)
+          return
+        }
+        queue.add(tryRow(row, tries + 1))
+      }
+      console.log('updated', row.Link)
+    }
+  }
+
+  for (const row of rows) {
+    if (!row.Link) {
+      continue
+    }
+    queue.add(tryRow(row))
+  }
+
+  queue.start()
+  await queue.onIdle()
+  console.log('finished.')
+}
+
+main()
