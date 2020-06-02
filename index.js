87dfc039e7fb7dfe6af79acdb89ff681d1934c41
Max Goodhart <c@chromakode.com>
2020-06-01 22:23:38 -0700

    Improve error handling, manual captcha entry, and retry

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const CREDS = require('./creds.json')
 
 const sleep = promisify(setTimeout)
 
+class CheckError extends Error {
+  constructor({captcha, retryable}, ...params) {
+    super(...params)
+    this.captcha = captcha
+    this.retryable = retryable
+  }
+}
+
 function findString(platformName, string) {
   return async function(page, url) {
     await page.goto(url)
@@ -28,7 +36,7 @@ const checkPeriscopeLive = findString('Periscope', `name="twitter:text:broadcast
 const checkYTLive = async function(page, url) {
   const result = await findString('YouTube', `liveStreamability`)(page, url)
   if (result.html.includes('Our systems have detected unusual traffic from your computer network.')) {
-    throw new Error('YouTube CAPTCHA required')
+    throw new CheckError({captcha: true}, 'YouTube CAPTCHA required')
   }
   const ytID = url.startsWith('https://youtu.be') ? url.split('youtu.be/')[1] : url.split('v=')[1]
   result.embed = `https://www.youtube.com/embed/${ytID}`
@@ -38,10 +46,13 @@ const checkYTLive = async function(page, url) {
 const checkFBLive = async function(page, url) {
   const result = await findString('Facebook', `"broadcast_status":"LIVE"`)(page, url)
   if (result.title === 'Security Check Required') {
-    throw new Error('Facebook CAPTCHA required')
+    throw new CheckError({captcha: true}, 'Facebook CAPTCHA required')
   }
   if (!result.html.includes('broadcast_status')) {
-    throw new Error('Facebook returned unexpected response')
+    throw new CheckError({retryable: true}, 'Facebook returned unexpected response')
+  }
+  if (result.title === 'You must log in to continue') {
+    throw new CheckError('Facebook content now private')
   }
   result.embed = `https://www.facebook.com/plugins/video.php?href=${url}&show_text=0`
   return result
@@ -96,12 +107,15 @@ async function main() {
           console.log('ratelimited. waiting 5s...')
           await sleep(10000)
           queue.start()
-        } else {
-          console.warn('error updating row', row.Link, err, 'waiting for nav...')
-	  await page.waitForNavigation({timeout: 2 * 60 * 1000})
+	  return
         }
 
-        if (tries > 3) {
+        console.warn('error updating row', row.Link, err, 'waiting for nav...')
+	if (err.captcha) {
+	  console.log('waiting for captcha...')
+	  await page.waitForNavigation({timeout: 2 * 60 * 1000})
+	}
+        if (!err.retryable || tries > 3) {
           console.warn('giving up on row', row.Link)
           return
         }
