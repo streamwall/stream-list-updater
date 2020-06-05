6bf03be289779ac51fc22310ee834e7bea5d8669
Max Goodhart <c@chromakode.com>
2020-06-04 23:18:16 -0700

    Support publishing from multiple sheets

--- link-publisher.js
+++ link-publisher.js
@@ -2,8 +2,7 @@
 const {GoogleSpreadsheet} = require('google-spreadsheet')
 const fetch = require('node-fetch')
 
-const FROM_SHEET_ID = process.env.FROM_SHEET_ID
-const FROM_TAB_NAME = process.env.FROM_TAB_NAME
+const [FROM_SHEET_ID, ...FROM_TAB_NAMES] = process.env.FROM_SHEETS.split('|').map(s => s.split(','))
 const TO_SHEET_ID = process.env.TO_SHEET_ID
 const TO_TAB_NAME = process.env.TO_TAB_NAME
 const ANNOUNCE_WEBHOOK_URL = process.env.ANNOUNCE_WEBHOOK_URL
@@ -13,40 +12,45 @@ const CREDS = require('./creds.json')
 const {doWithRetry, sleep} = require('./utils')
 
 async function runPublish() {
-  const fromDoc = new GoogleSpreadsheet(FROM_SHEET_ID)
-  await fromDoc.useServiceAccountAuth(CREDS)
-  await fromDoc.loadInfo()
-  const fromSheet = Object.values(fromDoc.sheetsById).find(s => s.title === FROM_TAB_NAME)
-  await fromSheet.loadHeaderRow()
-
   const toDoc = new GoogleSpreadsheet(TO_SHEET_ID)
   await toDoc.useServiceAccountAuth(CREDS)
   await toDoc.loadInfo()
   const toSheet = Object.values(toDoc.sheetsById).find(s => s.title === TO_TAB_NAME)
   await toSheet.loadHeaderRow()
 
-  const rows = await doWithRetry(() => fromSheet.getRows())
-  for (const row of rows) {
-    if (!row.Link || row.x === 'x') {
-      continue
+  const fromDoc = new GoogleSpreadsheet(FROM_SHEET_ID)
+  await fromDoc.useServiceAccountAuth(CREDS)
+  await fromDoc.loadInfo()
+
+  const fromSheets = Object.values(doc.sheetsById).filter(s => FROM_TAB_NAMES.includes(s.title))
+  for (const fromSheet of fromSheets) {
+    const rows = await doWithRetry(() => fromSheet.getRows())
+    for (const row of rows) {
+      if (!row.Link || row.x === 'x') {
+        continue
+      }
+
+      if (row.hasOwnProperty('Vetted') && row.Vetted !== 'x') {
+        continue
+      }
+
+      await doWithRetry(() => toSheet.addRow(row))
+      row.x = 'x'
+      await doWithRetry(() => row.save())
+
+      const resp = await fetch(ANNOUNCE_WEBHOOK_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: 'New Stream',
+          content: `**${row.Source}** — ${row.City}, ${row.State} (${row.Type}, ${row.View}) :link: <${row.Link}>${row.Notes ? ' ' + row.Notes : ''}`,
+        }),
+      })
+
+      console.log(`published ${row.Link}`)
     }
-
-    await doWithRetry(() => toSheet.addRow(row))
-    row.x = 'x'
-    await doWithRetry(() => row.save())
-
-    const resp = await fetch(ANNOUNCE_WEBHOOK_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: 'New Stream',
-        content: `**${row.Source}** — ${row.City}, ${row.State} (${row.Type}, ${row.View}) :link: <${row.Link}>${row.Notes ? ' ' + row.Notes : ''}`,
-      }),
-    })
-
-    console.log(`published ${row.Link}`)
   }
 }
 
